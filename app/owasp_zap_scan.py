8036aa5aa940381e5b06403e1d69b0a94f874c29
Aleksandr Boikov <skydiver.vrn@gmail.com>
2020-06-17 14:05:18 +0300

    edited scanner script

--- app/owasp_zap_scan.py
+++ app/owasp_zap_scan.py
@@ -5,11 +5,9 @@ import time
 import json
 import slack
 
-# sites_list = ["jibrel.com", "jwallet.network"]
 local_zap_folder = "/tmp/zap"
 docker_zap_folder = "/zap/wrk"
 zap_file_names = {}
-# slack_token = "xoxb-186470154240-786121597206-FVYtm6RW1utaQGEQZ5xCrEq7"
 slack_channel_index = "C014P63V8L8"
 site_list_file = "/app/sites_list.txt"
 slack_token = os.getenv('SLACK_TOKEN')
@@ -26,11 +24,11 @@ def make_zap_filenames():
 
 def run_tests():
     for site in sites_list:
+        print(zap_file_names[site])
+        print("starting docker")
         zap_command = "zap-baseline.py -t https://{} -j -a -m 1 -r {}.html -J {}.json".format(site,
                                                                                                  zap_file_names[site],
                                                                                                  zap_file_names[site])
-        print(zap_file_names[site])
-        print("starting docker")
         try:
             os.system(zap_command)
         except:
@@ -62,4 +60,8 @@ def send_slack_notify(slack_client, site, message, emoji):
 slack_client = slack.WebClient(slack_token)
 make_zap_filenames()
 run_tests()
+os.system("ls -la /")
+os.system("ls -la /zap")
+os.system("ls -la /app")
+os.system("ls -la /zap/wrk")
 analise_test_results()
